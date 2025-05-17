46dc8c1f1e779c20221391a24173cd2c0d6d4bd4
Aidan Flood <aidantf@gmail.com>
2025-05-16 20:51:12 -0400

    Real time time offset analysis

--- raspberry_pi/esp_device.py
+++ raspberry_pi/esp_device.py
@@ -1,10 +1,11 @@
 import struct
 import time
 import serial
+from config import *
 
 class ESPDevice:
-    def __init__(self, port, baud=115200, name=None):
-        self.ser = serial.Serial(port, baudrate=baud, timeout=2)
+    def __init__(self, port, baudrate=115200, name=None):
+        self.ser = serial.Serial(port, baudrate=baudrate, timeout=2)
         self.name = name or port
 
     def set_sampling_rate(self, rate_hz):
@@ -29,25 +30,27 @@ class ESPDevice:
         self.ser.write(b"CMD:SET\n" + struct.pack('<d', epoch))
 
     def read_samples(self, sampling_interval_ms=1.0):
+        self.ser.reset_input_buffer()  # Flushes any residual garbage before reading
         self.ser.write(b"CMD:READ\n")
         print(f"[{self.name}] Waiting for 4-byte base timestamp...")
-        time.sleep(0.01)
+        time.sleep(0.05)
         base_ts_raw = self.ser.read(4)
         print(f"[{self.name}] Got {len(base_ts_raw)} bytes for base_ts")
 
-        print(f"[{self.name}] Waiting for 400 bytes of data...")
-        time.sleep(0.01)
-        data_raw = self.ser.read(100 * 4)
+        print(f"[{self.name}] Waiting for {4 * BUFFER_SAMPLES} bytes of data...")
+        time.sleep(0.05)
+        data_raw = self.ser.read(BUFFER_SAMPLES * 4)
         print(f"[{self.name}] Got {len(data_raw)} bytes for data")
 
 
-        if len(base_ts_raw) < 4 or len(data_raw) < 100 * 4:
+        if len(base_ts_raw) < 4 or len(data_raw) < BUFFER_SAMPLES * 4:
             raise RuntimeError("Data read incomplete")
 
         base_ts = int.from_bytes(base_ts_raw, 'little')
-        values = list(struct.unpack('<100f', data_raw))
-        timestamps = [base_ts + i * sampling_interval_ms for i in range(100)]
-        return list(zip(timestamps, values))
+        values = list(struct.unpack(f'<{BUFFER_SAMPLES}f', data_raw))
+        timestamps = [base_ts + i * sampling_interval_ms for i in range(BUFFER_SAMPLES)]
+        # return list(zip(timestamps, values))
+        return base_ts, values
 
     def close(self):
         self.ser.close()
